d7800de4ef1b565a0977fdc6f8449dfab9bf6080
NatHan <133284439+NatHanNSilva12@users.noreply.github.com>
2023-05-12 08:43:27 -0300

    Add files via upload

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
     <>
     <div className='footer'>
         <div className='author'>
-        <p>Criado por <a href="docs.google.com/document/d/1E7zhlWK-4MBwGfX5JBsk96iJ7Bhttps://MOUsdJ/edit?usp=share_link&ouid=107708546702958327877&rtpof=true&sd=true" target="_blank" rel="noopener noreferrer">Nathan Silva</a> com <i className='far fa-heart'></i></p>
+        <p>Criado por <a href="https://docs.google.com/document/d/1puL4KnhWGPRps6I29dBcU2RgPNtBLMJd/edit?usp=share_link&ouid=107708546702958327877&rtpof=true&sd=true" target="_blank" rel="noopener noreferrer">Nathan Silva</a> com <i className='far fa-heart'></i></p>
         </div>
         <div className=''>
         <p className='copyright'> <i className='far fa-copyright'></i> 2023 All rights reserved</p>

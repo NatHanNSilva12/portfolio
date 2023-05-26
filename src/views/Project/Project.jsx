8e6b4b87f99a1dd140a0fa773ad3410f40b4a709
NatHan <133284439+NatHanNSilva12@users.noreply.github.com>
2023-05-26 11:30:27 -0300

    Add files via upload

--- src/views/Project/Project.jsx
+++ src/views/Project/Project.jsx
@@ -8,6 +8,7 @@ import 'aos/dist/aos.css'
 import ProjectBox from '../../components/ProjectBox/ProjectBox'
 import img7 from  '../../images/wotktale.png'
 import img9 from '../../images/site-raffa.png'
+import img8 from '../../images/site-finanças.png'
 
 
 function Project() {
@@ -24,6 +25,7 @@ function Project() {
           <div className="carousel owl-carousel">
             <ProjectBox title = "Jogo para o projeto Hackathon" img= {img7} link = "https://gamer0097br.itch.io/worktale"  />
             <ProjectBox title = "Site para um corretor imobiliário" img= {img9} link = "https://site-raffa.vercel.app/"  />
+            <ProjectBox title = "Site de controle financeiro" img= {img8} link = "https://controle-financeiro-black.vercel.app/"  />
           </div>
         </div>
       </div>
